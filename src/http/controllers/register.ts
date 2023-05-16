2733787b1b1c44a5c70a57c47327ae80e40cec61
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-16 15:43:12 -0400

    chore: create global error handler

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -23,7 +23,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       return reply.status(409).send({ message: error.message })
     }
 
-    return reply.status(500).send()
+    throw error
   }
 
   return reply.status(201).send()
