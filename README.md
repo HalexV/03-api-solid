6d942ec4cca5b3fcee287ed7bd768f9f242d543e
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-17 14:16:40 -0400

    feat: create get user profile use case

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ GymPass style app.
 São as funcionalidades da aplicação. O que vai ser possível o usuário fazer na nossa aplicação.
 
 - [x] Deve ser possível se cadastrar;
-- [] Deve ser possível se autenticar;
+- [x] Deve ser possível se autenticar;
 - [] Deve ser possível obter o perfil de um usuário logado;
 - [] Deve ser possível obter o número de check-ins realizados pelo usuário logado;
 - [] Deve ser possível o usuário obter seu histórico de check-ins;
---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,37 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.name).toBe('John Doe')
+  })
+
+  it('should not be able to get user profile with wrong id', async () => {
+    const promise = sut.execute({
+      userId: 'non-existing-id',
+    })
+
+    await expect(promise).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
