5eeed9733ddd03290de77b47668343a3c5ad202e
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-19 16:20:19 -0400

    feat: create jwt authentication

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -17,7 +17,20 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase()
 
-    await authenticateUseCase.execute({ email, password })
+    const { user } = await authenticateUseCase.execute({ email, password })
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+
+    return reply.status(200).send({
+      token,
+    })
   } catch (error) {
     if (error instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: error.message })
@@ -25,6 +38,4 @@ export async function authenticate(
 
     throw error
   }
-
-  return reply.status(200).send()
 }
