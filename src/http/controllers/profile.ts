e76681ed33708c74d1bcfb4f8baa07c1c62655a0
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-19 16:36:02 -0400

    feat: create verify jwt middleware

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,16 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  Reflect.deleteProperty(user, 'password_hash')
+
+  return reply.status(200).send({
+    user,
+  })
 }
