c75e8ed20ce222f6cd8bdcea11771cb7cb04c459
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-22 14:31:50 -0400

    feat: add role to jwt

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -20,7 +20,9 @@ export async function authenticate(
     const { user } = await authenticateUseCase.execute({ email, password })
 
     const token = await reply.jwtSign(
-      {},
+      {
+        role: user.role,
+      },
       {
         sign: {
           sub: user.id,
@@ -29,7 +31,7 @@ export async function authenticate(
     )
 
     const refreshToken = await reply.jwtSign(
-      {},
+      { role: user.role },
       {
         sign: {
           sub: user.id,
--- src/http/controllers/users/refresh.ts
+++ src/http/controllers/users/refresh.ts
@@ -4,8 +4,10 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   // Olha apenas para os cabeçalhos de cookies procurando o refresh token
   await request.jwtVerify({ onlyCookie: true })
 
+  const { role } = request.user
+
   const token = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
@@ -14,7 +16,7 @@ export async function refresh(request: FastifyRequest, reply: FastifyReply) {
   )
 
   const refreshToken = await reply.jwtSign(
-    {},
+    { role },
     {
       sign: {
         sub: request.user.sub,
