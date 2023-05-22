e827551c6ae99ad3771c2e43fd799357b09031ea
Halex Viotto Gomes <halexx002@gmail.com>
2023-05-22 14:33:19 -0400

    fix(test): pass admin parameter to create correct user

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,7 +1,21 @@
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyInstance } from 'fastify'
 import request from 'supertest'
 
-export async function createAndAuthenticateUser(app: FastifyInstance) {
+export async function createAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false,
+) {
+  await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
+  })
+
   await request(app.server).post('/users').send({
     name: 'John Doe',
     email: 'johndoe@example.com',
